82bbe538eb172415804c31069ef62c37f2eb8653
Bossarant <bgbondurant@gmail.com>
2020-11-09 18:49:39 -0800

    fixing the jquary made header

--- header.js
+++ header.js
@@ -1,1 +1,1 @@
-$('head').append('<header> <div class="header"> <a href="../Home" class="logo">Crimes Against Foodies</a> <div class="header-right"> <a href="../Home">Home</a> <a class="active" href="../about">About</a> <a href="../list">List</a> </div> </div> </header>')
+$('#head').append('<header> <div class="header"> <a href="../Home" class="logo">Crimes Against Foodies</a> <div class="header-right"> <a href="../Home">Home</a> <a class="active" href="../about">About</a> <a href="../list">List</a> </div> </div> </header>')
