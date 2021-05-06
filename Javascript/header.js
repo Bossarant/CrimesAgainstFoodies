b1de0aecbfc3a51cf1e44e0fcf8ea57c6900b475
Bossarant <bgbondurant@gmail.com>
2021-05-06 08:33:02 -0700

    Fixing Header.js

--- Javascript/header.js
+++ Javascript/header.js
@@ -3,15 +3,15 @@ $(function() {
     $('#head').prepend('<div class="header"></div>')
     $('.header').append('<a href="../Home" class="logo">Crimes Against Foodies</a>')
     $('.header').append('<div class="header-right"></div>')
-    if (pathname == "/About/", pathname == "/CrimesAgainstFoodies/About/") {
+    if (pathname == "/About/") {
         $('.header-right').append('<a href="../Home">Home</a>')
         $('.header-right').append('<a class="active" href="../About">About</a>')
         $('.header-right').append('<a href="../List">List</a>')
-    } else if (pathname == "/Home/", pathname == "/CrimesAgainstFoodies/Home/") {
+    } else if (pathname == "/Home/") {
         $('.header-right').append('<a class="active" href="../Home">Home</a>')
         $('.header-right').append('<a href="../About">About</a>')
         $('.header-right').append('<a href="../List">List</a>')
-    } else if (pathname == "/List/", pathname == "/CrimesAgainstFoodies/List/") {
+    } else if (pathname == "/List/") {
         $('.header-right').append('<a href="../Home">Home</a>')
         $('.header-right').append('<a href="../About">About</a>')
         $('.header-right').append('<a class="active" href="../List">List</a>')
